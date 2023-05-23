9baeeaf07325bbf6d8a0ed215ac9569a1b53673d
Viraj Amarasinghe <virajamara@gmail.com>
2023-05-23 09:15:52 +0530

    added  more api tests

--- tests/Feature/ProductApiTest.php
+++ tests/Feature/ProductApiTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Product;
 use Tests\TestCase;
 use App\Models\User;
 use Illuminate\Http\UploadedFile;
@@ -49,23 +51,6 @@ class ProductApiTest extends TestCase
             ]);;
     }
 
-    public function test_login_user(): void
-    {
-        $this->postJson('/api/v1/register', [
-            'name' => 'Vimal',
-            'email' => 'viraj@yahoo.com',
-            'password' => '123456',
-            'password_confirmation' => '123456'
-        ]);
-        $this->getJson('/api/v1/logout');
-        $response = $this->postJson('/api/v1/login', [
-            'email' => 'vimal@yahoo.com',
-            'password' => '123456',
-        ]);
-        $response
-            ->assertStatus(201);
-    }
-
     public function test_create_product_get_success_response(): void
     {
         $user = User::factory()->create();
@@ -90,17 +75,15 @@ class ProductApiTest extends TestCase
     public function test_get_product_get_success_response(): void
     {
         $user = User::factory()->create();
-        $this->assertCount(0, $user->tokens);
-
         $this->actingAs($user);
 
-        $this->postJson('/api/v1/products', [
+        $response1 = $this->postJson('/api/v1/products', [
             'name' => 'Biscuit',
             'description' => 'Biscuit Test',
             'price' => '250.32',
             'image' => UploadedFile::fake()->image('avatar.jpg')
         ]);
-        $response = $this->getJson('/api/v1/products/1');
+        $response = $this->getJson('/api/v1/products/' . json_decode($response1->getContent())->id);
         $response
             ->assertStatus(200);
     }
@@ -110,23 +93,97 @@ class ProductApiTest extends TestCase
         $user = User::factory()->create();
 
         $this->actingAs($user);
-        $this->postJson('/api/v1/products', [
+
+        Product::factory(10)->create();
+
+        $response1 = $this->postJson('/api/v1/products', [
             'name' => 'Biscuit',
             'description' => 'Biscuit Test',
             'price' => '250.32',
             'image' => UploadedFile::fake()->image('avatar.jpg')
         ]);
-        $response = $this->putJson('/api/v1/products/1', [
+
+        $response = $this->putJson('/api/v1/products/' . json_decode($response1->getContent())->id, [
             'name' => 'Biscuit',
             'description' => 'Biscuit Test',
-            'price' => '250.32',
+            'price' => '150.32',
         ]);
         $response
             ->assertStatus(201)
             ->assertJsonFragment([
                 'name' => 'Biscuit',
                 'description' => 'Biscuit Test',
-                'price' => '250.32'
+                'price' => '150.32'
             ]);
+    }
+
+    public function test_delete_product_get_no_content_response(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        Product::factory(10)->create();
+
+        $response1 = $this->postJson('/api/v1/products', [
+            'name' => 'Biscuit',
+            'description' => 'Biscuit Test',
+            'price' => '250.32',
+            'image' => UploadedFile::fake()->image('avatar.jpg')
+        ]);
+
+        $response = $this->deleteJson('/api/v1/products/' . json_decode($response1->getContent())->id);
+        $response
+            ->assertStatus(204);
+    }
+
+    public function test_recaptcha_get_validation_response(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response1 = $this->getJson('/api/v1/recaptcha/verify', [
+            'token' => 'testtoken',
+        ]);
+
+        $response1
+            ->assertStatus(422)
+            ->assertJsonFragment(
+                [
+                    "message" => "The token field is required.",
+                    "errors" => [
+                        "token" => [
+                            "The token field is required."
+                        ]
+                    ]
+                ]
+            );
+    }
+
+    public function test_get_all_products(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        Product::factory(10)->create();
+
+        $response = $this->getJson('/api/v1/products');
+        $response
+            ->assertStatus(200)
+            ->assertJsonCount(3);
+    }
+
+    public function test_get_all_products_with_filters(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response = $this->getJson('/api/v1/products?name=Bis');
+        $response
+            ->assertStatus(200)
+            ->assertJsonCount(3);
     }
 }
